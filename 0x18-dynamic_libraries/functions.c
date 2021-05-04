1be7f05b89c5993f32be2ef9c6dff51b05f58eeb
Blacksterhack <blastertv1001@gmail.com>
2021-05-03 23:05:15 -0500

    function

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,61 @@
+#include "op.h"
+
+/**
+ *add - adds two integers
+ *@a: is an int
+ *@b: is an int
+ *Return: addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *sub - substract two integers
+ *@a: is an int
+ *@b: is an int
+ *Return: substraction
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *mul - multiply two integers
+ *@a: is an int
+ *@b: is an int
+ *Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *div - divide two integers
+ *@a: is an int
+ *@b: is an int
+ *Return: division
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *mod - remainder of division of two integers
+ *@a: is an int
+ *@b: is an int
+ *Return: remainder
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
